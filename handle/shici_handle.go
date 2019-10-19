f2eb8cf93e1380ca69e0049fee7cae8a840f0bda
markuslee <markusleevip@outlook.com>
2019-10-20 00:24:36 +0800

    第三节

--- handle/shici_handle.go
+++ handle/shici_handle.go
@@ -3,9 +3,11 @@ package handle
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"github.com/markusleevip/go-shici/gofish"
 	"io"
 )
 
+var baseUrl ="https://so.gushiwen.org"
 type AuthorHandle struct{}
 func (h *AuthorHandle) Worker(body io.Reader,url string) {
 	doc, err:=goquery.NewDocumentFromReader(body)
@@ -17,5 +19,28 @@ func (h *AuthorHandle) Worker(body io.Reader,url string) {
 		fmt.Printf("%d author=%s\n",i,author)
 		link,_ := s.Attr("href")
 		fmt.Printf("%dlink=%s\n",i,link)
+
+		h := PoemHomeHandle{}
+		fish:=gofish.NewGoFish()
+		request,err := gofish.NewRequest("GET",baseUrl+link,gofish.UserAgent,&h,nil)
+		if err!=nil{
+			fmt.Println(err)
+			return
+		}
+		fish.Request = request
+		fish.Visit()
+	})
+}
+
+type PoemHomeHandle struct{}
+func (h *PoemHomeHandle) Worker(body io.Reader,url string) {
+	doc, err:=goquery.NewDocumentFromReader(body)
+	if err !=nil{
+		fmt.Errorf("doc.err.",err)
+	}
+	doc.Find(".sonspic").Find(".cont").Find("p").Find("a").Each(func(i int, s *goquery.Selection) {
+
+		link,_ := s.Attr("href")
+		fmt.Println("作品主页=",baseUrl+link)
 	})
 }
